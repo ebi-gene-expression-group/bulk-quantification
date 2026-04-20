d4b17773ddcca11beab7320ea83592e6d8521e9c
Anil Thanki <thanki.anil@gmail.com>
2026-04-20 11:45:58 +0100

    Refactor nextflow command in build_sortmerna_indices.sh
    Removed PROJECT_DIR variable and updated nextflow command.

--- bin/build_sortmerna_indices.sh
+++ bin/build_sortmerna_indices.sh
@@ -37,10 +37,9 @@ echo "IIII" | gzip >> ${WORK_DIR}/dummy_R1.fq.gz
 
 cp ${WORK_DIR}/dummy_R1.fq.gz ${WORK_DIR}/dummy_R2.fq.gz
 
-PROJECT_DIR="$(pwd)"
 
 # Run nf-core/rnaseq with only index building
-nextflow run ${PROJECT_DIR}/../subworkflows/rnaseq/main.nf \
+nextflow run ../subworkflows/rnaseq/main.nf \
     --input ${WORK_DIR}/dummy_samplesheet.csv \
     --outdir ${WORK_DIR}/output \
     --fasta ${GENOME_DIR} \
