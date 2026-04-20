5150679777bf254b8c37b2237b9a5addedc897ac
Anil Thanki <thanki.anil@gmail.com>
2026-04-20 11:33:08 +0100

    Create working directory for SortMeRNA index build

--- bin/build_sortmerna_indices.sh
+++ bin/build_sortmerna_indices.sh
@@ -14,6 +14,7 @@ INDEX_DIR="${REFERENCE_BASE}/contamination/sortmerna/index"
 
 # Create a temporary working directory
 WORK_DIR="${NF_WORKDIR}/ribo_index/"
+mkdir -p ${WORK_DIR}
 trap "rm -rf ${WORK_DIR}" EXIT
 
 echo "=== Building SortMeRNA Index ==="
